3e277eb1451d962e21eec1ffd2a892d5d1afa614
abinesh-yt <neshabinesh2008@gmail.com>
2026-06-10 14:38:21 +0530

    updated

--- script.js
+++ script.js
@@ -1,53 +1,92 @@
+let alreadyOpened = false;
+
 function showSurprise() {
+
+    if (alreadyOpened) return;
+
+    alreadyOpened = true;
 
     let name =
         document.getElementById("name").value;
 
-    document.getElementById("message").innerHTML =
-        `🎉 Happy Birthday ${name}! 🎂`;
+    if (name.trim() === "") {
 
-    createConfetti();
+        name = "My Friend";
+    }
+
+    let count = 3;
+
+    document.getElementById("countdown").innerHTML =
+        "🎉 Surprise in 3";
+
+    let timer = setInterval(() => {
+
+        count--;
+
+        if (count > 0) {
+
+            document.getElementById("countdown").innerHTML =
+                "🎉 Surprise in " + count;
+        }
+
+        else {
+
+            clearInterval(timer);
+
+            document.getElementById("countdown").innerHTML =
+                "";
+
+            document.getElementById("message").innerHTML =
+                `🎉 Happy Birthday ${name}! 🎂`;
+
+            createConfetti();
+
+            document.getElementById("surpriseBtn").innerHTML =
+                "🎉 Celebration Started";
+
+            document.getElementById("surpriseBtn").disabled =
+                true;
+        }
+
+    }, 1000);
 }
 
-let count = 3;
-
-let timer = setInterval(() => {
-    document.getElementById("countdown").innerHTML =
-        "🎉 Surprise in " + count;
-
-    count--;
-
-    if(count < 0){
-        clearInterval(timer);
-
-        document.getElementById("countdown").innerHTML =
-            "🎂 Happy Birthday!";
-    }
-},1000);
-
 function createConfetti() {
-    for(let i=0; i<20; i++) {
-        let confetti = document.createElement("div");
+
+    for (let i = 0; i < 25; i++) {
+
+        let confetti =
+            document.createElement("div");
 
         confetti.classList.add("confetti");
 
-        confetti.style.left = Math.random() * window.innerWidth + "px";
+        confetti.style.left =
+            Math.random() * window.innerWidth + "px";
 
         confetti.style.backgroundColor =
-            ["red", "blue", "yellow", "green", "purple", "orange"][
-                Math.floor(Math.random() * 6)
-            ];
+            ["red", "blue", "yellow", "green", "purple", "orange"]
+            [Math.floor(Math.random() * 6)];
 
         confetti.style.animationDuration =
-            Math.random() * 3 + 2 + "s";
+            Math.random() * 2 + 2 + "s";
 
         document.body.appendChild(confetti);
 
-       setTimeout(() => {
-    confetti.remove();
-    }, 3000);
-    }
+        setTimeout(() => {
 
-document.body.style.background =
-"radial-gradient(circle, #001133, #000814)";
+            confetti.remove();
+
+        }, 3000);
+    }
 }
+
+/* ENTER KEY SUPPORT */
+
+document.addEventListener("keydown",
+    function (event) {
+
+        if (event.key === "Enter") {
+
+            showSurprise();
+        }
+    });
