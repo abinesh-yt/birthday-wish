2eedfc3d3a69ea6dad5f1c14f545bb572b514c1a
abinesh-yt <neshabinesh2008@gmail.com>
2026-06-10 14:22:06 +0530

    version 2

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
-function showWish(){
+function showSurprise() {
+
+    let name =
+        document.getElementById("name").value;
 
     document.getElementById("message").innerHTML =
+        `🎉 Happy Birthday ${name}! 🎂`;
 
-    "🎉 May all your dreams come true! 🎉";
+    createConfetti();
+}
 
+function createConfetti() {
+    for (let i = 0; i < 50; i++) {
+        let confetti = document.createElement("div");
+
+        confetti.classList.add("confetti");
+
+        confetti.style.left = Math.random() * window.innerWidth + "px";
+
+        confetti.style.backgroundColor =
+            ["red", "blue", "yellow", "green", "purple", "orange"][
+                Math.floor(Math.random() * 6)
+            ];
+
+        confetti.style.animationDuration =
+            Math.random() * 3 + 2 + "s";
+
+        document.body.appendChild(confetti);
+
+        setTimeout(() => {
+            confetti.remove();
+        }, 5000);
+    }
 }
